3738ab9d53f019741920805f257772112dafb2b1
spear21 <llspear@charter.net>
2021-10-11 11:40:44 -0400

    testing build

--- src/controllers/raceController.ts
+++ src/controllers/raceController.ts
@@ -5,6 +5,38 @@ import { Connect,  Query} from '../config/mysql';
 
 const NAMESPACE = "RACE";
 
+const addRace = (req: Request, res: Response, next: NextFunction) => {
+    logging.info(NAMESPACE, "Getrting all races");
+    let { id, name, maxAge, minAge, speed, description } = req.body
+    let query = `Insert Into Race (id, name, maxAge, minAge, speed, description) VALUES ("${id}", "${name}", "${maxAge}","${minAge}", "${speed}", "${description}" )`;
+    Connect()
+    .then(connection => {
+        Query(connection, query)
+        .then(result => {
+            return res.status(200).json({
+                result
+            })
+        })
+        .catch(error => {
+            logging.error(NAMESPACE, error.message, error);
+            return res.status(500).json({
+                message: error.message,
+                error
+            });
+        })
+        .finally(() => {
+            connection.end();
+        })
+    })
+    .catch(error => {
+        logging.error(NAMESPACE, error.message, error);
+        return res.status(500).json({
+            message: error.message,
+            error
+        })
+    });
+};
+
 const getALlRaces = (req: Request, res: Response, next: NextFunction) => {
     logging.info(NAMESPACE, "Getrting all races");
 
@@ -38,6 +70,73 @@ const getALlRaces = (req: Request, res: Response, next: NextFunction) => {
   
 };
 
+const getRaceById = (req: Request, res: Response, next: NextFunction) => {
+    logging.info(NAMESPACE, "Getting this race");
+    let id = req.params.id;
+    let query = `SELECT * FROM Race WHERE id=${req.params.id}`;
+    Connect()
+    .then(connection => {
+        Query(connection, query)
+        .then(results => {
+            return res.status(200).json({
+                results
+            })
+        })
+        .catch(error => {
+            logging.error(NAMESPACE, error.message, error);
+            return res.status(500).json({
+                message: error.message,
+                error
+            });
+        })
+        .finally(() => {
+            connection.end();
+        })
+    })
+    .catch(error => {
+        logging.error(NAMESPACE, error.message, error);
+        return res.status(500).json({
+            message: error.message,
+            error
+        })
+    });
+  
+};
+
+const updateRaceById = (req: Request, res: Response, next: NextFunction) => {
+    logging.info(NAMESPACE, "Getting this race");
+    let { id, name, maxAge, minAge, speed, description } = req.body
+    let query = `UPDATE Race SET (name, maxAge, minAge, speed, description) VALUES ("${name}", "${maxAge}", "${minAge}", "${speed}", "${description}")WHERE id=${ req.params.id}`;
+    Connect()
+    .then(connection => {
+        Query(connection, query)
+        .then(results => {
+            return res.status(200).json({
+                results
+            })
+        })
+        .catch(error => {
+            logging.error(NAMESPACE, error.message, error);
+            return res.status(500).json({
+                message: error.message,
+                error
+            });
+        })
+        .finally(() => {
+            connection.end();
+        })
+    })
+    .catch(error => {
+        logging.error(NAMESPACE, error.message, error);
+        return res.status(500).json({
+            message: error.message,
+            error
+        })
+    });
+  
+};
 
 
-export default { getALlRaces };
+
+
+export default { addRace, getALlRaces, getRaceById, updateRaceById };

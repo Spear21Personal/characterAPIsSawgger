952ec7f2d2939a997d2e8c3bc2c6e163280af3c0
spear21 <llspear@charter.net>
2021-10-20 13:55:55 -0400

    update DB connection

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,22 +1,39 @@
+import Mongoose from "mongoose";
+import dotenv from "dotenv";
 
-import "reflect-metadata";
-import {createConnection} from "typeorm";
+let database: Mongoose.Connection;
 
 
-createConnection().then(async connection => {
+export const connect = () => {
+    dotenv.config();
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xselj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+    if (database) {
+        return;
+    }
+    
+    Mongoose.connect( uri, {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    });
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+    database = Mongoose.connection;
 
-}).catch(error => console.log(error));
+    database.on("open", async () => {
+        console.log('DB conbnected');
+    });
+    database.on("error", () => {
+        console.log("Error connecting to DB")
+    });
+
+}
+
+export const disconnect = () => {
+    if (!database) {
+        return;
+    }
+    Mongoose.disconnect;
+}
